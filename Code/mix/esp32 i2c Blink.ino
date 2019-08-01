4faae340f8314cf07bc0ea9fbec2a00f9a35a58e
Poisson48 <42474010+Poisson48@users.noreply.github.com>
2019-08-01 23:27:19 +0200

    Create esp32 i2c Blink.ino

---
+++ Code/mix/esp32 i2c Blink.ino
@@ -0,0 +1,99 @@
+/*!
+ * @file  DFRobot_SHT20_test.ino
+ * @brief DFRobot's SHT20 Humidity And Temperature Sensor Module
+ * @n     This example demonstrates how to read the user registers to display resolution and other settings.
+ *        Uses the SHT20 library to display the current humidity and temperature.
+ *        Open serial monitor at 9600 baud to see readings.
+ *        Errors 998 if not sensor is detected. Error 999 if CRC is bad.
+ * Hardware Connections:
+ * -VCC = 3.3V
+ * -GND = GND
+ * -SDA = A4 (use inline 330 ohm resistor if your board is 5V)
+ * -SCL = A5 (use inline 330 ohm resistor if your board is 5V)
+ */
+
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+
+
+
+#include <Wire.h>
+#include "DFRobot_SHT20.h"
+
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "b6AVUeDglh7Ualx527BbZo5f6TVNbq8J";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "🦐";
+char pass[] = "1524xlh1524xlh";
+
+// This function will be called every time Slider Widget
+// in Blynk app writes values to the Virtual Pin V1
+
+
+
+DFRobot_SHT20    sht20;
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("hey ! i'm booting !!");
+    sht20.initSHT20();                                  // Init SHT20 Sensor
+    delay(100);
+    sht20.checkSHT20();                                 // Check SHT20 Sensor
+    Blynk.begin(auth, ssid, pass);
+
+
+    ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+    float humd = sht20.readHumidity();                  // Read Humidity
+    float temp = sht20.readTemperature();               // Read Temperature
+    Blynk.virtualWrite(V0, temp);
+    Blynk.virtualWrite(V1, humd);
+    Blynk.run();
+    ArduinoOTA.handle();
+}
